1db0e8c60c2bdb1519b9ccbab34b040f3c11df31
tsaristbomba <andrexerdna@gmail.com>
2020-04-20 08:09:43 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { Route, Switch } from "react-router-dom"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navbar from "./components/Navbar"
+import Home from "./components/Home"
+import RandomGif from "./components/RandomGif"
+import Search from "./components/Search"
+
+export default function App() {
+
+    return (
+        <div>
+            <Navbar />
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+
+                <Route path="/random">
+                    <RandomGif />
+                </Route>
+
+                <Route path="/search">
+                    <Search />
+                </Route>
+            </Switch>
+        </div>
+    )
 }
-
-export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react"
+require('dotenv').config()
+
+export default function Home() {
+    const [trending, setTrend] = useState([])
+    const [slicedArr, setSliced] = useState([])
+    const [load, setLoad] = useState(8)
+    const [loading, setLoading] = useState(true)
+
+    const api_key = process.env.REACT_APP_API_KEY
+
+
+    useEffect(() => {
+        console.log(api_key)
+        setup()
+    }, [])
+
+    useEffect(() => {
+        setSliced(trending.slice(0, load))
+    }, [trending, load])
+
+    async function fetchTrending() {
+        let trend = []
+        try {
+            const response = await fetch(
+                `https://api.giphy.com/v1/gifs/trending?api_key=${api_key}&limit=80&rating=G`
+            )
+            const json = await response.json()
+            console.log(json)
+            trend = await json.data.map(res => res.images.fixed_height_downsampled.url)
+        } catch(err) {
+            console.log(err)  
+        }
+        return trend
+    }
+
+    async function setup() {
+        try {
+            const gifData = await fetchTrending()
+            setTrend(gifData.map(data => data))
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    const handleLoad = () => {
+        setLoad(load + 8)
+    }
+
+    return(
+        <div className="row home-container mx-auto mt-5">
+            <div className="col-md-12 text-info text-center mb-3">
+                <h2>Trending Gifs</h2>
+            </div>
+            <div className="col-12 mx-auto text-center mb-5">
+                {
+                    loading ? <h3>loading...</h3> : slicedArr.map((data, key) => <img className="trend m-2" key={key} alt="" src={data}></img>)
+                }
+            </div>
+            {
+                trending.length > load && <button className="btn btn-outline-secondary mb-5" style={{boxShadow: 'none'}} onClick={handleLoad}>Load More</button>
+            }
+        </div>
+    )
+}
