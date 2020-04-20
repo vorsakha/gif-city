bc5425e0e73c237e5a67c127dd3c5a5333a2403b
tsaristbomba <andrexerdna@gmail.com>
2020-04-20 08:36:58 -0300

    Updates

---
+++ precache-manifest.9a5d848518dd6ed55fbed08ef1d8f5ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ddf670be7001ca9da21f85846019f25",
+    "url": "/gif-city/index.html"
+  },
+  {
+    "revision": "4aa414cf61a62cad6d3f",
+    "url": "/gif-city/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "317045e9fdec8ffff2b4",
+    "url": "/gif-city/static/css/main.5dcec67a.chunk.css"
+  },
+  {
+    "revision": "4aa414cf61a62cad6d3f",
+    "url": "/gif-city/static/js/2.1ec29de2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gif-city/static/js/2.1ec29de2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "317045e9fdec8ffff2b4",
+    "url": "/gif-city/static/js/main.0138f7c3.chunk.js"
+  },
+  {
+    "revision": "262734a602ac63c14636",
+    "url": "/gif-city/static/js/runtime-main.6bfe9e35.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.08e49cbbd1de4fde2811df01782acf44.js"
+  "/gif-city/precache-manifest.9a5d848518dd6ed55fbed08ef1d8f5ff.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gif-city/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
