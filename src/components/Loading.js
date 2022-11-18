ccaf556b70875197932f241257cf5fca83e2476d
Andrei Ferreira <andreitf.dev@gmail.com>
2022-11-18 13:33:38 -0300

    feat: create a Loading component

---
+++ src/components/Loading.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import loadingGif from '../img/load.gif';
+
+const Loading = () => {
+  return <img className="loading" src={loadingGif} alt="loading..." />;
+};
+
+export default Loading;
